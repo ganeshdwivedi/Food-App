51c4b8add92a52421f9e23b50bc483d82e5f9a91
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2024-01-19 13:09:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    headers() {
-        return [
-            {
-                source: '/api/data',
-                headers: [
-                    { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' }
-                ]
-            }
-        ];
-    }
 }
 
 module.exports = nextConfig
