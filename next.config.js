d497eb7072e444ffde60e61945356a11b4d92abd
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2024-01-19 12:54:28 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    headers() {
+        return [
+            {
+                source: '/api/data',
+                headers: [
+                    { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' }
+                ]
+            }
+        ];
+    }
+}
 
 module.exports = nextConfig
