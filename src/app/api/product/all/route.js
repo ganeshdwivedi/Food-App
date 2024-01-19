16539c8dbff73d95cbac4bbeb9f7d055f4db6224
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2024-01-19 12:56:02 +0530

    Update route.js

--- src/app/api/product/all/route.js
+++ src/app/api/product/all/route.js
@@ -2,10 +2,13 @@ import connect from "../../../../config/dbconfig";
 import Product from "../../../../model/productModel";
 import { NextResponse } from "next/server";
 
-export async function POST(req) {
+export async function get(req) {
   await connect();
   try {
     const products = await Product.find({});
+    NextResponse.headers = {
+  'Cache-Control': 'no-cache, no-store, must-revalidate'
+};
     return NextResponse.json({ products: products });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
